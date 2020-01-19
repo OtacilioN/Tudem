bb21df205d0e48755227c360ad1664c4b4d6960b
OtacilioN <osmn@cesar.org.br>
2020-01-19 09:00:11 -0500

    Updates

---
+++ precache-manifest.be9d2d0780afa58ecd15638da08d7dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a68437340b34072bf3a54b309994c32",
+    "url": "/Tudem/index.html"
+  },
+  {
+    "revision": "4c376fd298778024e46f",
+    "url": "/Tudem/static/css/main.8020ef43.chunk.css"
+  },
+  {
+    "revision": "bb8b33057ba543879fbd",
+    "url": "/Tudem/static/js/2.660cbc16.chunk.js"
+  },
+  {
+    "revision": "1ea325708a90ed11e205e5fe91d972ee",
+    "url": "/Tudem/static/js/2.660cbc16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c376fd298778024e46f",
+    "url": "/Tudem/static/js/main.f61070e0.chunk.js"
+  },
+  {
+    "revision": "44ccc5e1d3a0b2700d13",
+    "url": "/Tudem/static/js/runtime-main.3eb01c40.js"
+  }
+]);
